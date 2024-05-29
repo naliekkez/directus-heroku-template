837ebfa2779ed14870fbb0469f72fef4ac0a88d7
Kobe Marchal <work@kobemarchal.be>
2024-05-29 09:24:21 +0200

    temp: disable redis

--- directus.config.js
+++ directus.config.js
@@ -3,9 +3,9 @@ module.exports = {
   DB_CONNECTION_STRING:
     process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL +
     '?sslmode=no-verify',
-  REDIS: process.env.REDIS_URL,
-  RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
-  CACHE_REDIS: process.env.CACHE_REDIS || process.env.REDIS_URL,
+  // REDIS: process.env.REDIS_URL,
+  // RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
+  // CACHE_REDIS: process.env.CACHE_REDIS || process.env.REDIS_URL,
   EMAIL_FROM: process.env.EMAIL_FROM || process.env.MAILGUN_SMTP_LOGIN,
   EMAIL_SMTP_HOST:
     process.env.EMAIL_SMTP_HOST || process.env.MAILGUN_SMTP_SERVER,
